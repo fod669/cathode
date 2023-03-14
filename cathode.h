b2e0cf8d97a20bb929ed2b5fc4e49b0970209792
fod669 <fod669@gmail.com>
2023-03-14 23:59:38 +1100

    Added the main cathode.h header file.

---
+++ cathode.h
@@ -0,0 +1,20 @@
+
+#pragma once
+#ifndef CATHODE_H_
+#define CATHODE_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdalign.h>
+
+// Make sure cth_defines.h is always first.
+#include "include/cth_defines.h"
+
+#if CTH_C_VER < CTH_C11
+	#error Need to compile with c11 or higher!
+#endif
+
+// All other cathode lib includes go here.
+
+#endif // CATHODE_H_
+
