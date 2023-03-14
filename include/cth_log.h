cc4f81cb3db4cf8e24a737f465b834804a011e1b
fod669 <fod669@gmail.com>
2023-03-15 01:12:14 +1100

    Added the logging header for reals this time.

---
+++ include/cth_log.h
@@ -0,0 +1,45 @@
+
+#pragma once
+#ifndef CTH_LOG_H_
+#define CTH_LOG_H_
+
+#if CTH_LOG_LINE_INFO
+	// These print line info, so you can double click the error in the output window to go to the source location.
+	#define log_info(_Format, ...)			log_printf_dbg(LOG_INFO, __FILE__, __LINE__, _Format, ##__VA_ARGS__)
+	#define log_warning(_Format, ...)		log_printf_dbg(LOG_WARNING, __FILE__, __LINE__, _Format, ##__VA_ARGS__)
+	#define log_error(_Format, ...)			log_printf_dbg(LOG_ERROR, __FILE__, __LINE__, _Format, ##__VA_ARGS__)
+	#define log_critical(_Format, ...)		log_printf_dbg(LOG_CRITICAL, __FILE__, __LINE__, _Format, ##__VA_ARGS__)
+
+	// These are the "no line info" versions, so you can still print messages without line info in debug mode if you wish.
+	#define log_info_nli(_Format, ...)		log_printf(LOG_INFO, _Format, ##__VA_ARGS__)
+	#define log_warning_nli(_Format, ...)	log_printf(LOG_WARNING, _Format, ##__VA_ARGS__)
+	#define log_error_nli(_Format, ...)		log_printf(LOG_ERROR, _Format, ##__VA_ARGS__)
+	#define log_critical_nli(_Format, ...)	log_printf(LOG_CRITICAL, _Format, ##__VA_ARGS__)
+#else
+	#define log_info(_Format, ...)			log_printf(LOG_INFO, _Format, ##__VA_ARGS__)
+	#define log_warning(_Format, ...)		log_printf(LOG_WARNING, _Format, ##__VA_ARGS__)
+	#define log_error(_Format, ...)			log_printf(LOG_ERROR, _Format, ##__VA_ARGS__)
+	#define log_critical(_Format, ...)		log_printf(LOG_CRITICAL, _Format, ##__VA_ARGS__)
+
+	#define log_info_nli					log_info
+	#define log_warning_nli					log_warning
+	#define log_error_nli					log_error
+	#define log_critical_nli				log_critical
+#endif
+
+typedef enum eLogErrorLevel
+{
+	LOG_INFO,
+	LOG_WARNING,
+	LOG_ERROR,
+	LOG_CRITICAL,
+
+	LOG_LEVEL_COUNT
+} eLogErrorLevel;
+
+void log_printf(eLogErrorLevel errorLevel, const char* format, ...) STBSP__ATTRIBUTE_FORMAT(2, 3);
+void log_printf_dbg(eLogErrorLevel errorLevel, const char* file, u32 line, const char* format, ...) STBSP__ATTRIBUTE_FORMAT(4, 5);
+void log_printfv(eLogErrorLevel errorLevel, const char* format, va_list args);
+
+#endif // CTH_LOG_H_
+
